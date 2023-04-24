e09c0663ed8ba81a616135f1f0676162fc8aaf6e
PabloPS0 <pablopierry.github.98@gmail.com>
2023-04-24 00:44:18 -0300

    Alteração na função da data de publicação

--- enquetes/models.py
+++ enquetes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_next = models.CharField(max_length=200)
-    pub_date = models.DateTimeFields("date published")
+    pub_date = models.DateTimeFields(default=timezone.now, blank=True)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
