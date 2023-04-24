b42b2b99ef6cbf874ac2e48aab1c70465c923062
PabloPS0 <pablopierry.github.98@gmail.com>
2023-04-24 00:53:15 -0300

    Alteração dos modelos: Criação de novos e armazenamento dessas alterações como migration

--- enquetes/models.py
+++ enquetes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_next = models.CharField(max_length=200)
-    pub_date = models.DateTimeFields(default=timezone.now, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
